21cc29599383b661139ffa5bd937b9aa94108f48
Adrian <adrianvalverde45@gmail.com>
2023-07-02 00:08:54 -0400

    commit tree 696db85b7a0f0c9c65c525696bf6ea39daec2e5e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+   has_many :characters
+   belongs_to :network
+   has_many :actors, through: :characters
+
+   def actors_list
+    self.actors.map do |a|
+        "#{a.first_name} #{a.last_name}"
+    end
+   end
+
 end
